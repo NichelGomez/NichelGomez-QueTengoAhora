9f09dd54d974d807bcd74e00f19992a91b11f5bd
NichelGomez <nichel8080@gmail.com>
2022-09-30 13:17:35 +0100

    Ejercicio 1.22

---
+++ EnlaceParaHorario.php
@@ -0,0 +1,13 @@
+    <html>
+        <head>
+            <title>Página previa horario</title>
+        </head>
+        <body>
+        <?php
+            include 'QueTengoAhora.php';
+            queTengoAhora();
+        ?>
+            <a target="_blank" href="HorarioExterno.php">Clic aquí para mostrar el horario completo.</a>
+        </body>
+    </html>
+        
